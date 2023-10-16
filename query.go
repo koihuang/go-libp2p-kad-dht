e18f700212111d8c7cd85ed2419a6eac4f73118b
koi <38130681+koihuang@users.noreply.github.com>
2023-10-16 12:40:17 +0800

    test: test relay

--- query.go
+++ query.go
@@ -413,8 +413,9 @@ func (q *query) queryPeer(ctx context.Context, ch chan<- *queryUpdate, p peer.ID
 		//	return
 		//}
 
-		relayPeer := q.queryPeers.GetReferrer(p)
-		relayAddr := fmt.Sprintf("/p2p/%s/p2p-circuit/ipfs/%s", relayPeer, p.String())
+		//relayPeer := q.queryPeers.GetReferrer(p)
+		//relayAddr := fmt.Sprintf("/p2p/%s/p2p-circuit/ipfs/%s", relayPeer, p.String())
+		relayAddr := fmt.Sprintf("/p2p-circuit/ipfs/%s", p.String())
 		maAddr, err := ma.NewMultiaddr(relayAddr)
 		if err != nil {
 			ch <- &queryUpdate{cause: p, unreachable: []peer.ID{p}}
